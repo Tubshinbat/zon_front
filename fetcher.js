42c82f290c3257810a3d69c45189dbb8a2b887ba
Tubshinbat~ <tuvshinbat596@gmail.com>
2023-01-23 07:12:42 +0800

    a

--- fetcher.js
+++ fetcher.js
@@ -1,7 +1,5 @@
 const fetcher = async (url, options = { cache: "no-cache" }) => {
-  const res = await fetch(url, {
-    next: { revalidate: 10 },
-  });
+  const res = await fetch(url, { cache: "no-store" });
 
   if (!res.ok) {
     const error = new Error("An error occurred while fetching the data.");
