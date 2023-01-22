4355878892a037245472443edbd8d204e32802e5
Tubshinbat~ <tuvshinbat596@gmail.com>
2023-01-23 05:27:14 +0800

    base change

--- axios-base.js
+++ axios-base.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8060/api/v1/",
+  baseURL: "https://beta.metaldoor.mn/api/",
 });
 
 instance.defaults.withCredentials = true;
